47f11439a6adb082eea301b63031092916c5cc9f
alfaqi <alfaqiabdullah@gmail.com>
2024-12-30 21:02:30 +0200

    Update the backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import path from "path";
 import productRoutes from "./routes/products.route.js";
 
 dotenv.config();
@@ -9,18 +8,9 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // allows us to accept JSON data in body
 
-const __dirname = path.resolve();
-
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/products", productRoutes);
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   connectDB();
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-dotenv.config();
 
-const HOST = process.env.HOST;
-console.log("HOST:", HOST);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: HOST,
-      },
-    },
+  define: {
+    VITE_REACT_APP_BACKEND_BASEURL: JSON.stringify(
+      process.env.VITE_REACT_APP_BACKEND_BASEURL
+    ),
   },
 });
