4c14017a419902d9f3b6ca36c1ba1acdf5ba35df
alfaqi <alfaqiabdullah@gmail.com>
2024-12-30 13:07:16 +0200

    feat: Add toast

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "./components/ui/provider";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider>
+        <Toaster />
         <App />
       </Provider>
     </BrowserRouter>
