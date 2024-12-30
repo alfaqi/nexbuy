c07f08c60171049085cd64da772aca55a470f125
alfaqi <alfaqiabdullah@gmail.com>
2024-12-30 21:08:43 +0200

    Update the backend

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import productRoutes from "./routes/products.route.js";
+import cors from "cors"
 
 dotenv.config();
-
 const app = express();
 app.use(express.json()); // allows us to accept JSON data in body
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
