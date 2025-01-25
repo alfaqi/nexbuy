3ee7403acedbbc10fde1600198e14c7d4feec173
alfaqi <alfaqiabdullah@gmail.com>
2025-01-25 23:00:27 +0200

    Update Dark Mode

--- frontend/src/components/Navs/TopNav.jsx
+++ frontend/src/components/Navs/TopNav.jsx
@@ -1,13 +1,15 @@
 import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
-import { BsMoon, BsPlusSquare, BsSun } from "react-icons/bs";
+import { BsMoon, BsSun } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import CreateModal from "../Modals/CreateModal";
 import SearchBar from "../SearchBar";
 import UserInfo from "./UserInfo";
+import { useColorMode } from "../ui/color-mode";
+import { useUserStore } from "@/store/users";
 
 export default function TopNav() {
-  const colorMode = "";
-
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { user } = useUserStore();
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -24,23 +26,19 @@ export default function TopNav() {
           fontWeight={"bold"}
           textTransform={"uppercase"}
           textAlign={"center"}
-          // bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          // bgGradient="to-r" gradientFrom="red.200" gradientTo="blue.200"
-          // bgClip={"text"}
         >
-          <Link to="/">Product Store </Link>
+          <Link to="/">Product Store</Link>
         </Text>
-        <HStack gap={2} alignItems={"center"}>
-          {/* <Link to="/create">
-            <Button>
-              <BsPlusSquare size={20} />
+        {user && (
+          <HStack gap={2} alignItems={"center"}>
+            <SearchBar />
+            <CreateModal />
+            <Button onClick={toggleColorMode} aria-label="Toggle Dark Mode">
+              {colorMode === "light" ? <BsMoon /> : <BsSun />}
             </Button>
-          </Link> */}
-          <SearchBar />
-          <CreateModal />
-          <Button>{colorMode === "light" ? <BsMoon /> : <BsSun />}</Button>
-          <UserInfo />
-        </HStack>
+            <UserInfo />
+          </HStack>
+        )}
       </Flex>
     </Container>
   );
