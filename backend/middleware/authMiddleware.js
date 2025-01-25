39281f0c420c373a4bc15a00496d1994b938a40d
alfaqi <alfaqiabdullah@gmail.com>
2025-01-25 17:46:08 +0200

    Add JWT Auth

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const authenticate = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, error: "Access denied. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ success: false, error: "Invalid token." });
+  }
+};
