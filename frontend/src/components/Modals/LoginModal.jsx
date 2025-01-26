bb08bf9dd085b1b7fcdb577c8baec4e19b39402b
alfaqi <alfaqiabdullah@gmail.com>
2025-01-26 14:47:29 +0200

    Cleanup LoginModal

--- frontend/src/components/Modals/LoginModal.jsx
+++ frontend/src/components/Modals/LoginModal.jsx
@@ -1,19 +1,8 @@
-import { useState } from "react";
+import { Button, DialogTrigger, useDisclosure } from "@chakra-ui/react";
 import {
-  Button,
-  DialogTrigger,
-  Input,
-  useDisclosure,
-  VStack,
-} from "@chakra-ui/react";
-import { useProductStore } from "@/store/product";
-import { toaster } from "@/components/ui/toaster";
-import {
-  DialogActionTrigger,
   DialogBody,
   DialogCloseTrigger,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogRoot,
   DialogTitle,
@@ -23,41 +12,6 @@ import { FiLogIn } from "react-icons/fi";
 
 export default function LoginModal() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { createProduct } = useProductStore();
-
-  const [newProduct, setNewProduct] = useState({
-    name: "",
-    price: "",
-    image: "",
-    description: "",
-  });
-  const [isCreating, setIsCreating] = useState(false);
-
-  const handleAddProduct = async () => {
-    try {
-      setIsCreating(true);
-      const { success, message } = await createProduct(newProduct);
-      console.log("message:", message);
-
-      const title = success === true ? "Success" : "Error";
-      const type = success === true ? "success" : "error";
-
-      toaster.create({
-        title: title,
-        description: message,
-        type: type,
-      });
-      setNewProduct({ name: "", price: "", image: "" });
-    } catch (error) {
-      toaster.create({
-        title: "Error",
-        description: error.message,
-        type: "error",
-      });
-    } finally {
-      setIsCreating(false);
-    }
-  };
 
   return (
     <DialogRoot open={isOpen} onOpenChange={onClose}>
